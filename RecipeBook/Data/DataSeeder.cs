2081d5ebae79c841e58e8d19194e1992dfce59d7
alex-ogn <alex_ogn111@abv.bg>
2024-05-28 00:43:59 +0300

    Add simple roles

---
+++ RecipeBook/Data/DataSeeder.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RecipeBook.Data
+{
+    public static class DataSeeder
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                context.Database.EnsureCreated();
+
+                // Seed roles
+                SeedRoles(roleManager).Wait();
+
+                // Seed users
+                SeedUsers(userManager).Wait();
+            }
+        }
+
+        private static async Task SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (!await roleManager.RoleExistsAsync("Admin"))
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+            if (!await roleManager.RoleExistsAsync("User"))
+            {
+                await roleManager.CreateAsync(new IdentityRole("User"));
+            }
+        }
+
+        private static async Task SeedUsers(UserManager<IdentityUser> userManager)
+        {
+            if (userManager.FindByEmailAsync("admin@example.com").Result == null)
+            {
+                IdentityUser user = new IdentityUser
+                {
+                    UserName = "admin@example.com",
+                    Email = "admin@example.com"
+                };
+
+                IdentityResult result = userManager.CreateAsync(user, "Admin123!").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Admin").Wait();
+                }
+            }
+        }
+    }
+
+}
