55cb17830164fce57bfba2fe2abcaf87d5756229
alex-ogn <alex_ogn111@abv.bg>
2025-06-05 11:27:59 +0300

    add custom account pages

---
+++ RecipeBook/ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace RecipeBook.ViewModels.Account
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Имейл")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Парола")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Потвърди паролата")]
+        [Compare("Password", ErrorMessage = "Паролите не съвпадат.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
